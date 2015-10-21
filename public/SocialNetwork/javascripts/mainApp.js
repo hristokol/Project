acff4f346e1b14ba53988cb243c751c4b13c3df3
Hristo Kolev <hristogk1993@abv.bg>
2015-10-21 11:51:51 +0300

    Added new responsive design

---
+++ public/SocialNetwork/javascripts/mainApp.js
@@ -0,0 +1,9 @@
+'use strict'
+var app = angular.module('SocialNetwork', ['ngRoute','ngAnimate']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+$routeProvider.
+    when('/profile',{templateUrl:'SocialNetwork/profile.htm',controller:'profileController'}).
+    when('/',{templateUrl:'SocialNetwork/home.html',controller:'pageController'}).
+    otherwise('/');
+}]);
---
+++ public/SocialNetwork/javascripts/profileController.js
@@ -0,0 +1,4 @@
+'use strict'
+app.controller('profileController', function ($scope) {
+$scope.pageClass='profile-page';
+});

05da0aed71ab7b76791d631e011c7fc6742b9960
Hristo Kolev <hristogk1993@abv.bg>
2015-10-22 16:23:10 +0300

    Bug fixes

--- public/SocialNetwork/javascripts/mainApp.js
+++ public/SocialNetwork/javascripts/mainApp.js
@@ -1,4 +1,15 @@
 'use strict'
+var app = angular.module('SocialNetwork', ['ngRoute', 'ngAnimate']);
+
+app.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+        when('/profile', {templateUrl: 'SocialNetwork/profile.htm', controller: 'profileController'}).
+        when('/', {templateUrl: 'SocialNetwork/home.html', controller: 'pageController'}).
+        when('/postModal',{redirectTo:'/'}).
+        otherwise('/');
+}]);
+
+
 var app = angular.module('SocialNetwork', ['ngRoute','ngAnimate']);
 
 app.config(['$routeProvider', function ($routeProvider) {
--- public/SocialNetwork/javascripts/pageController.js
+++ public/SocialNetwork/javascripts/pageController.js
@@ -1,4 +1,25 @@
 'use strict'
-app.controller('pageController', function ($scope) {
+
+app.controller('pageController', function ($rootScope, $scope) {
+    $scope.onlineFriends = [];
+
+    for (var i = 0; i < 20; i++) {
+        if (i % 2 == 0) {
+            $scope.onlineFriends.push({avatar: 'images/nissan.jpg', name: 'Friend', surname: 'Friend'});
+        } else {
+            $scope.onlineFriends.push({avatar: 'images/avatar1.jpg', name: 'Friend', surname: 'Friend'});
+
+        }
+    }
+
+    $scope.$on('$routeChangeSuccess', function () {
+        $rootScope.pageLoaded = true;
+    });
+
+    $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        if (next.$$route.originalPath == '/postModal' && current && current.$$route.originalPath == '/') {
+            event.preventDefault();
+        }
+    });
 
 });
